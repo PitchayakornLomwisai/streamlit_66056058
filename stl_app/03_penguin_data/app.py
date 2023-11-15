c3bc51b9a58ef53e3e0754ab4ae4c8a2c6de0689
peacechyk <95404776+peacechyk@users.noreply.github.com>
2023-11-15 23:05:56 +0700

    main

--- stl_app/03_penguin_data/app.py
+++ stl_app/03_penguin_data/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib as plt
 import seaborn as sns
 
 st.title("Palmer's Penguins")
@@ -13,12 +13,12 @@ choices = ['bill_length_mm',
 
 # https://docs.streamlit.io/library/api-reference/widgets/st.selectbox
 # 1. สร้าง st.selectbox ของ ตัวเลือก แกน x และ y จาก choices
-selected_x_var = 'อะไรดี'
-selected_y_var = 'อะไรดี'
+selected_x_var = st.selectbox('เลือก แกน x', (choices))
+selected_y_var =  st.selectbox('เลือก แกน y', (choices))
 
 # https://docs.streamlit.io/library/api-reference/widgets/st.file_uploader
 # 2. สร้าง st.file_uploader เพื่อให้เลือกไฟล์ .csv เท่านั้น จากเครื่องผู้ใช้งาน
-penguin_file = None
+penguin_file = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
 
 if penguin_file is not None:
     penguins_df = pd.read_csv(penguin_file)
